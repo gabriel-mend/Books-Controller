23f7b066f9d67884a6633ca96095cad0c2ecb1fa
Gabriel <gabriel@Gabriel-Mac-mini.local>
2020-05-09 20:25:19 -0300

    Conectando o backend com o frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api'
+
 import {
   GlobalStyle, 
   Container,
@@ -11,6 +13,43 @@ import {
 import { Edit, Trash2, } from 'react-feather'
 
 function App() {
+  const [books, setBooks] = useState([])
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [price, setPrice] = useState('')
+  const [launch, setLaunch] = useState('')
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    const data = {
+      title,
+      author,
+      price,
+      launch
+    }
+    try {
+      const response = await api.post('books', data)
+    } catch (err) {
+        alert(`Erro no cadastro tente novamente`)
+    }
+  }
+
+  async function handleDeleteBook(id) {
+    try {
+      await api.delete(`books/${id}`)
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    api.get('books')
+      .then(response => {
+        setBooks(response.data)
+      })
+  }, [books])
+
   return (
       <Container>
         <GlobalStyle blackColor/>
@@ -24,42 +63,52 @@ function App() {
             <th>Preço</th>
             <th>Ações</th>
           </tr>
-          <tr>
-            <td>auhsdgfyasbdf</td>
-            <td>hasdgfyuasdf</td>
-            <td>hasdgfyuasdf</td>
-            <td>Germany</td>
-            <td>Germany</td>
-            <td>
-              <ButtonUpdate>
-                <Edit size={20}/>
-              </ButtonUpdate>
-              <ButtonDelete>
-                <Trash2 size={20}/>
-              </ButtonDelete>
-            </td>
+          {books.map(book => (
+            <tr>
+              <td>{book.sku}</td>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td>{book.launch.slice(0, 10)}</td>
+              <td>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'}).format(parseFloat(book.price))}</td>
+              <td>
+                <ButtonUpdate>
+                  <Edit size={20}/>
+                </ButtonUpdate>
+                <ButtonDelete onClick={() => handleDeleteBook(book._id)}>
+                  <Trash2 size={20}/>
+                </ButtonDelete>
+              </td>
           </tr>
+          ))}
         </Table>
-        <BookForm>
+        <BookForm onSubmit={handleSubmit}>
             Adicionar novo livro
             <div>
               <input 
                 type="text" 
                 placeholder="Título do livro"
-                required 
+                required
+                value={title}
+                onChange={e => setTitle(e.target.value)}
               />
               <input 
                 type="text" 
                 placeholder="Autor do livro"
-                required  
+                required 
+                value={author}
+                onChange={e => setAuthor(e.target.value)} 
               />
               <input 
                 type="text" 
                 placeholder="Preço do livro"
-                required  
+                required
+                value={price}
+                onChange={e => setPrice(e.target.value)}  
               />
               <input 
                 type="date" 
+                value={launch}
+                onChange={e => setLaunch(e.target.value)}
               />
               <button>Adicionar</button>
             </div>
